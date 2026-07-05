45dfcf52bf41d16bda22056ad58c1a7c5997a52c
Alessandro Torres <aletl10@hotmail.com>
2026-07-05 14:41:20 -0700

    ci: usar github actions para el despliegue automático

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ReactLab/', // <-- AGREGA ESTA LÍNEA (Respeta las mayúsculas de tu repo)
   plugins: [react()],
   resolve:{
   alias:{
